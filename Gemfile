34ba9a203889c83627c561617371b54122e6e5ed
Mark Kok <mark.kok1019@gmail.com>
2017-03-15 22:00:20 +0100

    add faker to generate company names, create company factory test

--- Gemfile
+++ Gemfile
@@ -42,6 +42,7 @@ group :development, :test do
   gem 'byebug', platform: :mri
   gem 'rspec-rails', '~> 3.5'
   gem 'factory_girl_rails'
+  gem 'faker'
 end
 
 group :development do
--- spec/factories/companies.rb
+++ spec/factories/companies.rb
@@ -1,5 +1,7 @@
 FactoryGirl.define do
   factory :company do
-    
+    name { Faker::Company.name }
+    created_at '2017-03-15 21:05:32'
+    updated_at '2017-03-15 21:05:32'
   end
 end
